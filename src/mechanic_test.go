ff304acfbfb26a3f6ad62725f267efb3a133d957
Flaouve <flaouve@gmail.com>
2026-06-23 15:20:23 +0300

    test: add initial tests for eat and damage functions (WIP

---
+++ src/mechanic_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestEat(t *testing.T) {
+
+	my := &Pet{
+		Life:   true,
+		Hunger: 50,
+	}
+	my.eat(20)
+	expect := 30
+
+	if my.Hunger != expect {
+		t.Errorf("The eat() function didn't reduce hunger correctly! Expected: %d, Actual: %d", expect, my.Hunger)
+	}
+}
+
+func TestDamage(t *testing.T) {
+	my := &Pet{
+		Life:    true,
+		Hunger:  80,
+		Healthy: 100,
+	}
+	my.damage(10)
+	expect := 90
+
+	if my.Healthy != expect {
+		t.Errorf("The demage() func didnt reduce hunger correctly! Expected: %d, Actual: %d", expect, my.Healthy)
+	}
+}
